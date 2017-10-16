41a44e4caaed85f93e510fd2f9b1edf5a6bf240c
sjkim <skim952@uwo.ca>
2017-10-16 18:37:36 +0000

    example

--- Lab2/script.js
+++ Lab2/script.js
@@ -1,1 +1,8 @@
 // JavaScript File
+
+
+var mylist = document.getElementById("fruits");
+var entry = document.createElement('li');
+entry.appendChild(document.createTextNode("whatisthis"));
+mylist.appendChild(entry);
+//
